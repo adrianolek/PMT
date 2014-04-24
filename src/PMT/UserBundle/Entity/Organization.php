88ad3eaa9762e91c07735843805fe65b8a0a5392
Adrian Olek <adrianolek@gmail.com>
2014-04-24 23:24:55 +0200

    Organization entity

---
+++ src/PMT/UserBundle/Entity/Organization.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace PMT\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Organization
+ *
+ * @ORM\Table(name="pmt_organizations")
+ * @Gedmo\SoftDeleteable(fieldName="deleted_at")
+ * @ORM\Entity(repositoryClass="PMT\UserBundle\Entity\OrganizationRepository")
+ */
+class Organization
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $created_at;
+
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $updated_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $deleted_at;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Organization
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ src/PMT/UserBundle/Entity/OrganizationRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PMT\UserBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class OrganizationRepository extends EntityRepository
+{
+}
