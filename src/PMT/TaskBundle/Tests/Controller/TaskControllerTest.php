2b1ccc69948c1d347f13e8911dbbb3636de59f87
Adrian Olek <adrianolek@gmail.com>
2014-05-15 22:43:09 +0200

    Added task create test

--- src/PMT/TaskBundle/Tests/Controller/TaskControllerTest.php
+++ src/PMT/TaskBundle/Tests/Controller/TaskControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace PMT\TaskBundle\Tests\Controller;
 
+use Doctrine\ORM\EntityManager;
 use PMT\TestBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
 
 class TaskControllerTest extends WebTestCase
 {
@@ -20,8 +22,34 @@ class TaskControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $link = $crawler->selectLink('Foo Project')->link();
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testAddTask()
+    {
+        $client = static::createAuthClient();
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $project = $em->createQueryBuilder()->select('p')->from('PMT\ProjectBundle\Entity\Project', 'p')->getQuery()->getSingleResult();
+
+        $tasks_url = '/project/' . $project->getId() . '/tasks';
+        $crawler = $client->request('GET', $tasks_url);
+        
+        $link = $crawler->selectLink('Add task')->link();
+        
         $crawler = $client->click($link);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Create')->form();
+
+        $client->submit($form, array(
+            'task[name]' => 'Foo project',
+            'task[category]' => 'feature'
+        ));
+
+        $this->assertTrue($client->getResponse()->isRedirect($tasks_url));
     }
 }
