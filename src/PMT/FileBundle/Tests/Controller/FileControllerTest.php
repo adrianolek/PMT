73e6c19aee9bc4ae3066e3ac48d71e5151f1ed94
Adrian Olek <adrianolek@gmail.com>
2014-05-26 22:59:16 +0200

    Added file upload test

---
+++ src/PMT/FileBundle/Tests/Controller/FileControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PMT\FileBundle\Tests\Controller;
+
+use Doctrine\ORM\EntityManager;
+use PMT\TestBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileControllerTest extends WebTestCase
+{
+
+    public function setUp()
+    {
+        $this->loadFixtures(array(
+            'PMT\UserBundle\DataFixtures\ORM\LoadUserData',
+            'PMT\ProjectBundle\DataFixtures\ORM\LoadProjectData',
+            'PMT\TaskBundle\DataFixtures\ORM\LoadTaskData',
+        ));
+    }
+    
+    public function testAdd()
+    {
+        $client = static::createAuthClient();
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $task = $em->createQueryBuilder()->select('t')->from('PMT\TaskBundle\Entity\task', 't')->getQuery()->getSingleResult();
+
+        $crawler = $client->request('GET', '/task/' . $task->getId() . '/file/new');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Upload')->form();
+
+        $client->submit($form, array(
+            'form[file]' => new UploadedFile(
+                'src/PMT/FileBundle/DataFixtures/File/test.txt', 'test.txt'
+            )
+        ));
+
+        $this->assertTrue($client->getResponse()->isRedirect('/project/' . $task->getProject()->getId() . '/task/' . $task->getId()));
+    }
+}
