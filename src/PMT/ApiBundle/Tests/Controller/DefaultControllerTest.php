44ff863cbfc2247f5183cab5e628d013d20d0098
Adrian Olek <adrianolek@gmail.com>
2014-06-01 21:01:55 +0200

    Added tasks api tests

--- src/PMT/ApiBundle/Tests/Controller/DefaultControllerTest.php
+++ src/PMT/ApiBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,7 +10,9 @@ class DefaultControllerTest extends WebTestCase
     public function setUp()
     {
         $this->loadFixtures(array(
-            'PMT\UserBundle\DataFixtures\ORM\LoadUserData'
+            'PMT\UserBundle\DataFixtures\ORM\LoadUserData',
+            'PMT\ProjectBundle\DataFixtures\ORM\LoadProjectData',
+            'PMT\TaskBundle\DataFixtures\ORM\LoadTaskData',
         ));
     }
 
@@ -36,6 +38,47 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createApiClient();
 
         $client->request('GET', '/api/project.json');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+    
+    public function testTasks()
+    {
+        $client = static::createApiClient();
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $project = $em->createQueryBuilder()->select('p')->from('PMT\ProjectBundle\Entity\Project', 'p')->getQuery()->getSingleResult();
+        
+        $client->request('GET', '/api/project/'.$project->getId().'/task.json');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTask()
+    {
+        $client = static::createApiClient();
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $task = $em->createQueryBuilder()->select('t')->from('PMT\TaskBundle\Entity\Task', 't')->getQuery()->getSingleResult();
+
+        $client->request('GET', '/api/project/'.$task->getProject()->getId().'/task/'.$task->getId().'.json');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEstimate()
+    {
+        $client = static::createApiClient();
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $task = $em->createQueryBuilder()->select('t')->from('PMT\TaskBundle\Entity\Task', 't')->getQuery()->getSingleResult();
+
+        $client->request('POST', '/api/task/'.$task->getId().'/estimate.json', array(
+            'time' => 4,
+        ));
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
