91bd8d5e48fb2cb4e507f70f99a3cdebf5e05c9d
Adrian Olek <adrianolek@gmail.com>
2014-08-28 23:29:39 +0200

    Updated tracking tests

--- src/PMT/TrackingBundle/Tests/Controller/TrackingControllerTest.php
+++ src/PMT/TrackingBundle/Tests/Controller/TrackingControllerTest.php
@@ -21,9 +21,6 @@ class TrackingControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createAuthClient();
-        $client->request('GET', '/tracking');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         /** @var $em EntityManager */
@@ -41,8 +38,10 @@ class TrackingControllerTest extends WebTestCase
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         /** @var $em EntityManager */
         $task = $em->createQueryBuilder()->select('t')->from('PMT\TaskBundle\Entity\Task', 't')->setMaxResults(1)->getQuery()->getSingleResult();
+        $user = $em->createQueryBuilder()->select('u')->from('PMT\UserBundle\Entity\User', 'u')->setMaxResults(1)->getQuery()->getSingleResult();
 
-        $crawler = $client->request('GET', '/tracking');
+        $trackingPath = '/user/' . $user->getId() . '/tracking';
+        $crawler = $client->request('GET', $trackingPath);
         $link = $crawler->selectLink('Add entry')->link();
         $crawler = $client->click($link);
 
@@ -61,13 +60,18 @@ class TrackingControllerTest extends WebTestCase
             'track[startTime]' => '08:00',
             'track[endTime]' => '09:00',
         ));
-        $this->assertTrue($client->getResponse()->isRedirect('/tracking'));
+        $this->assertTrue($client->getResponse()->isRedirect($trackingPath));
     }
 
     public function testEdit()
     {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $user = $em->getRepository('PMTUserBundle:User')->findOneBy(array('email' => 'manager@pmt.test'));
+        $trackingPath = '/user/' . $user->getId() . '/tracking';
+        
         $client = static::createAuthClient();
-        $crawler = $client->request('GET', '/tracking?date_start=2014-01-01&date_end=2014-01-31');
+        $crawler = $client->request('GET', $trackingPath.'?date_start=2014-01-01&date_end=2014-01-31');
         $link = $crawler->selectLink('Edit')->link();
         $crawler = $client->click($link);
 
@@ -82,16 +86,21 @@ class TrackingControllerTest extends WebTestCase
         $form['track[date]'] = '2014-01-02';
         $client->submit($form);
 
-        $this->assertTrue($client->getResponse()->isRedirect('/tracking'));
+        $this->assertTrue($client->getResponse()->isRedirect($trackingPath));
     }
 
     public function testDelete()
     {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var $em EntityManager */
+        $user = $em->getRepository('PMTUserBundle:User')->findOneBy(array('email' => 'manager@pmt.test'));
+        $trackingPath = '/user/' . $user->getId() . '/tracking';
+        
         $client = static::createAuthClient();
-        $crawler = $client->request('GET', '/tracking?date_start=2014-01-01&date_end=2014-01-31');
+        $crawler = $client->request('GET', $trackingPath.'?date_start=2014-01-01&date_end=2014-01-31');
         $link = $crawler->selectLink('Delete')->link();
         $crawler = $client->click($link);
 
-        $this->assertTrue($client->getResponse()->isRedirect('/tracking'));
+        $this->assertTrue($client->getResponse()->isRedirect($trackingPath));
     }
 }
